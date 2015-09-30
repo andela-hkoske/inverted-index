96ba17a9238c6ee95ad21ac529cc8f9b5ac98629
hannahkoske <hannah.koske@andela.com>
2015-09-30 15:44:08 +0300

    Re-submission: Fixed nitpicks in inverted-index.js

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -7,11 +7,10 @@
 // PROPERTIES:
 //    path of json file, index of json file, results of search on index, json values of json File and reference to itself
 // METHODS:
-//    createIndex(path, cb): to create an index given the path of the json file and a callback 
+//    createIndex(path, cb): to create an index given the path of the json file and a callback
 //    for when asynchronous call is done executing
-//    getIndex(): returns the current index as an object but also accepts the path of a jason 
+//    getIndex(): returns the current index as an object but also accepts the path of a jason
 //    file and returns the index
-//    printIndex(): prints index to console
 //    searchIndex(): searches the index for the terms specified as arguments
 //    checkItem(): checks whether the item passed as an argument exists in the index
 
@@ -27,14 +26,12 @@ function Index() {
     var request = new XMLHttpRequest();
     request.open('GET', path, true);
     request.onreadystatechange = function(aEvt) {
-      if (request.readyState == 4) {
-        if (request.status == 200) {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
           obj.jsonValues = returnJsonData(request.responseText);
           obj.index = showOccurences(obj.jsonValues, removePunctuation(removeDuplicates(toWords(obj.jsonValues))));
-          console.log(obj.index);
           finished();
-        } else
-          console.log("Error loading request\n");
+        }
       }
     };
     request.send(null);
@@ -45,38 +42,30 @@ function Index() {
       this.createIndex(arguments[0],arguments[1]);
     }
     return {
-      "index": this.index
+      index: this.index
     };
   };
 
-  this.printIndex = function() {
-    for (var item of this.index) {
-      console.log(item[0] + " : " + item[1] + " , " + item[2]);
-    }
-  };
-
   this.searchIndex = function(terms) {
-    if (typeof(terms) == "object") {
+    if (typeof(terms) === "object") {
       terms = toLower(terms);
       this.results = searching(this.jsonValues, this.index, terms);
-      console.log(obj.results);
     } else {
       var args = [];
       for (var item in arguments) {
         args.push(arguments[item]);
       }
-      console.log(args);
       args = toLower(args);
       this.results = searching(this.jsonValues, this.index, args);
-      console.log(obj.results);
     }
     return this.results;
   };
 
   this.checkItem = function(term) {
     for (var item of this.index) {
-      if (item.indexOf(term) != -1)
+      if (item.indexOf(term) !== -1){
         return true;
+      }
     }
     return false;
   };
@@ -87,7 +76,6 @@ var returnJsonData = function(jsonFile) {
   var jsonValues = [];
   JSON.parse(jsonFile, function(k, v) {
     if (k === "text" || k === "title") {
-      //console.log("Key:" + k, "Value:" + v);
       jsonValues.push(v);
     }
   });
@@ -95,12 +83,14 @@ var returnJsonData = function(jsonFile) {
   return jsonValues;
 };
 
-//  checks whether the item/index passed is empty 
+//  checks whether the item/index passed is empty
 var checkEmpty = function(item) {
-  if (item.length === 0)
+  if (item.length === 0){
     return true;
-  else
+  }
+  else{
     return false;
+  }
 };
 
 //  converts an array of words from their current case to lower case
@@ -122,7 +112,7 @@ var toWords = function(jsonFile) {
   // appends the words to uniqueWords
   for (var i = 0; i < jsonFile.length; i++) {
     var words = jsonFile[i][1].trim().split(" ");
-    while (words.indexOf("") != -1) {
+    while (words.indexOf("") !== -1) {
       words.splice(words.indexOf(""), 1);
     }
     words = toLower(words);
@@ -142,17 +132,17 @@ var removeDuplicates = function(uniqueWords) {
   for (var j = 0; j < uniqueWords.length; j++) {
     firstOcc = j;
     lastOcc = uniqueWords.lastIndexOf(uniqueWords[j]);
-    if (firstOcc != lastOcc) {
+    if (firstOcc !== lastOcc) {
       uniqueWords.splice(firstOcc + 1, lastOcc - firstOcc);
     }
   }
   return uniqueWords;
 };
 
-// removes basic punctuation from items of an array of words 
+// removes basic punctuation from items of an array of words
 var removePunctuation = function(uniqueWords) {
   for (var i = 0, n = uniqueWords.length; i < n; i++) {
-    uniqueWords[i] = (uniqueWords[i].replace(',', '')).replace('.', '').replace('/', '').replace('?', '').replace('\"', '').replace('\'', '');
+    uniqueWords[i] = uniqueWords[i].replace(/[,?."'/\\]/g,'');
   }
   return uniqueWords;
 };
@@ -170,7 +160,7 @@ var showOccurences = function(jsonFile, uniqueWords) {
       occ;
     for (var l = 0, m = jsonFile.length; l < m; l++) {
       occ = jsonFile[l][1].trim().toLowerCase().search([uniqueWords[k]]);
-      if (occ != -1) {
+      if (occ !== -1) {
         wordOcc.push(true);
       } else {
         wordOcc.push(false);
@@ -197,21 +187,23 @@ var searching = function(json, index, terms) {
     for (var item of terms) {
       var subResults = [];
       for (var i = 0, n = index.length; i < n; i++) {
-        if (index[i][0].search(item) != -1) {
-          console.log(index[i][0]+" : "+item);
-          if (index[i][1] === true)
+        if (index[i][0].search(item) !== -1) {
+          if (index[i][1] === true){
             subResults.push(0);
-          if (index[i][2] === true)
+          }
+          if (index[i][2] === true){
             subResults.push(1);
-          if (subResults.length === 2)
+          }
+          if (subResults.length === 2){
             break;
+          }
         }
       }
-      if (subResults.length == 0)
+      if (subResults.length === 0){
         subResults.push("Not found");
+      }
       results.push(subResults);
     }
     return results;
-  } else
-    return undefined;
+  }
 };
